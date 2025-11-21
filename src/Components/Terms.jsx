231e5089d1dea0bd31fe41dc678743b914cced77
aria <rb.guerre@gmail.com>
2025-11-21 01:36:33 +0100

    update term OK

--- src/Components/Terms.jsx
+++ src/Components/Terms.jsx
@@ -13,6 +13,8 @@ function Terms() {
 
     const updateDialogRef = useRef(null);
     const [tempUpdateId, setTempUpdateId] = useState(-1);
+    const [updateTermName, setUpdateTermName] = useState("");
+    const [updateTypeValue, setUpdateTypeValue] = useState(null);
 
     useEffect(() => {
         handleFetchTermList();
@@ -43,7 +45,8 @@ function Terms() {
         } else {
             postDialogRef.current.close();
         }
-        resetDialogValues();
+        setPostTypeValue(TermTypes.TEXT);
+        setPostTermName("");
     };
 
     const handlePostTerm = () => {
@@ -75,23 +78,47 @@ function Terms() {
         handlePostModalOpening(false);
     };
 
-    const resetDialogValues = () => {
-        setPostTypeValue(TermTypes.TEXT);
-        setPostTermName("");
-    }
-
     // UPDATE TERM
     const handleUpdateModalOpening = (isOpen, id) => {
         setTempUpdateId(id);
         if (isOpen) {
+            const item = termList.find(t => t.id === id);
+            setUpdateTermName(item.name);
+            setUpdateTypeValue(item.type);
             updateDialogRef.current.showModal();
         } else {
+            setUpdateTermName("");
+            setUpdateTypeValue(null);
             updateDialogRef.current.close();
         }
     };
 
-    const handleTermUpdate = (id) => {
-        
+    const handleUpdateTerm = () => {
+        fetch(
+            `${import.meta.env.VITE_API_BASE_URL}terms/${tempUpdateId}`,
+            {
+                method: "PUT",
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    name: updateTermName,
+                    type: updateTypeValue
+                })
+            }
+        )
+            .then((res) => res.json())
+            .then((data) => setTermList(
+                (previousState) => previousState.map(
+                    (term) => term.id === data.result.id ? {...term, name: data.result.name, type: data.result.type} : {...term}
+                )
+            ))
+            .catch((error) => {
+                console.error(error);
+            });
+
+        handlePostModalOpening(false);
     }
 
     return (
@@ -108,7 +135,7 @@ function Terms() {
                     }
                 </ul>
                 <p>
-                    Voici la liste des termes disponibles :
+                    Voici le catalogue des termes disponibles :
                 </p>
 
                 {
@@ -137,11 +164,15 @@ function Terms() {
                 {
                     isTermConnectionAvailable
                         ? (
-                            <button className="hoverReactBlue" onClick={() => handlePostModalOpening(true)}>
+                            <button className="hoverReactBlue inlineSpacedElements" onClick={() => handlePostModalOpening(true)}>
                                 Créer un nouveau terme
                             </button>
                         ) : null
                 }
+
+                <button className="hoverReactBlue inlineSpacedElements" onClick={handleFetchTermList}>
+                    Recharger le catalogue de termes
+                </button>
 
                 <dialog ref={postDialogRef}>
                     <p className="dialogBox">Veuillez définir le nom et le type du terme que vous souhaitez créer :</p>
@@ -204,16 +235,76 @@ function Terms() {
                         </div>
                         <div className="dialogBox">
                             <button className="hoverReactBlue inlineSpacedElements" type="submit">OK</button>
-                            <button className="hoverReactRed inlineSpacedElements" type="button" onClick={() => handlePostModalOpening(false, -1)}>Annuler</button>
+                            <button className="hoverReactRed inlineSpacedElements" type="button" onClick={() => handlePostModalOpening(false)}>Annuler</button>
                         </div>
                     </form>
                 </dialog>
 
                 <dialog ref={updateDialogRef}>
-                    <p>Veuillez définir le nom et le type du terme que vous souhaitez mettre à jour :</p>
-                    <form method="dialog">
-                        <button className="hoverReactBlue">OK</button>
-                        <button className="hoverReactRed" autoFocus onClick={() => handleUpdateModalOpening(false)}>Annuler</button>
+                    <p className="dialogBox">Veuillez définir le nom et le type du terme que vous souhaitez mettre à jour :</p>
+                    <form method="dialog" onSubmit={handleUpdateTerm}>
+                        <div className="dialogBox">
+                            <label className="inlineSpacedElements" htmlFor="updateTermName">Nom :</label>
+                            <input
+                                className="inlineSpacedElements"
+                                type="text"
+                                id="updateTermName"
+                                name="name"
+                                required
+                                maxLength="50"
+                                size="16"
+                                value={updateTermName}
+                                onChange={(e) => setUpdateTermName(e.target.value)}
+                            />
+                        </div>
+                        <div className="dialogBox">
+                            <fieldset>
+                                <legend>
+                                    Type :
+                                </legend>
+
+                                <div>
+                                    <input
+                                        type="radio"
+                                        id="updateTermTypeText"
+                                        name="type"
+                                        value={TermTypes.TEXT}
+                                        checked={updateTypeValue === TermTypes.TEXT}
+                                        onChange={() => setUpdateTypeValue(TermTypes.TEXT)}
+                                    />
+                                    <label htmlFor={TermTypes.TEXT}>Texte</label>
+                                </div>
+
+                                <div>
+                                    <input
+                                        type="radio"
+                                        id="updateTermTypeNumber"
+                                        name="type"
+                                        value={TermTypes.NUMBER}
+                                        checked={updateTypeValue === TermTypes.NUMBER}
+                                        onChange={() => setUpdateTypeValue(TermTypes.NUMBER)}
+                                    />
+                                    <label htmlFor={TermTypes.NUMBER}>Nombre</label>
+                                </div>
+
+                                <div>
+                                    <input
+                                        type="radio"
+                                        id="updateTermTypeDate"
+                                        name="type"
+                                        value={TermTypes.DATE}
+                                        checked={updateTypeValue === TermTypes.DATE}
+                                        onChange={() => setUpdateTypeValue(TermTypes.DATE)}
+                                    />
+                                    <label htmlFor={TermTypes.DATE}>Date</label>
+                                </div>
+                                
+                            </fieldset>
+                        </div>
+                        <div className="dialogBox">
+                            <button className="hoverReactBlue inlineSpacedElements" type="submit">OK</button>
+                            <button className="hoverReactRed inlineSpacedElements" type="button" onClick={() => handleUpdateModalOpening(false, -1)}>Annuler</button>
+                        </div>
                     </form>
                 </dialog>
 
